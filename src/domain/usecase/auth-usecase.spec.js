7f3217a46e024f47efc58c3873cc08d925e21e39
Ian Coelho <ian.coel@hotmail.com>
2022-07-22 23:54:03 -0300

    chore: Ensure AuthUseCase calls Encrypter with the correct values

--- src/domain/usecase/auth-usecase.spec.js
+++ src/domain/usecase/auth-usecase.spec.js
@@ -9,13 +9,25 @@ const makeSut = () => {
     }
   }
 
+  class EncrypterSpy {
+    async compare (password, hashedPassword) {
+      this.password = password
+      this.hashedPassword = hashedPassword
+    }
+  }
+
+  const encrypterSpy = new EncrypterSpy()
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
-  loadUserByEmailRepositorySpy.user = {}
-  const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
+  loadUserByEmailRepositorySpy.user = {
+    password: 'hashed_password'
+  }
+
+  const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy)
 
   return {
     sut,
-    loadUserByEmailRepositorySpy
+    loadUserByEmailRepositorySpy,
+    encrypterSpy
   }
 }
 
@@ -61,5 +73,12 @@ describe('Auth UseCase', () => {
     const { sut } = makeSut()
     const acessToken = await sut.auth('any_email@email.com', 'invalid_password')
     expect(acessToken).toBeNull()
+  })
+
+  test('Should call Encrypter with the correct values', async () => {
+    const { sut, loadUserByEmailRepositorySpy, encrypterSpy } = makeSut()
+    await sut.auth('any_email@email.com', 'any_password')
+    expect(encrypterSpy.password).toBe('any_password')
+    expect(encrypterSpy.hashedPassword).toBe(loadUserByEmailRepositorySpy.user.password)
   })
 })
