67085832c9f2f6970783a68ce680c5ed44d1e7e0
Ian Coelho <ian.coel@hotmail.com>
2022-07-23 00:17:10 -0300

    chore: Move LoadUserByRepository and Encrypter to factory helpers

--- src/domain/usecase/auth-usecase.spec.js
+++ src/domain/usecase/auth-usecase.spec.js
@@ -1,7 +1,23 @@
 const { MissingParamError } = require('../../utils/errors')
 const { AuthUseCase } = require('./auth-usecase')
 
-const makeSut = () => {
+const makeEncrypter = () => {
+  class EncrypterSpy {
+    async compare (password, hashedPassword) {
+      this.password = password
+      this.hashedPassword = hashedPassword
+
+      return this.isValid
+    }
+  }
+
+  const encrypterSpy = new EncrypterSpy()
+  encrypterSpy.isValid = true
+
+  return encrypterSpy
+}
+
+const makeLoadUserByEmailRepository = () => {
   class LoadUserByEmailRepositorySpy {
     async load (email) {
       this.email = email
@@ -9,25 +25,24 @@ const makeSut = () => {
     }
   }
 
-  class EncrypterSpy {
-    async compare (password, hashedPassword) {
-      this.password = password
-      this.hashedPassword = hashedPassword
-    }
-  }
-
-  const encrypterSpy = new EncrypterSpy()
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
   loadUserByEmailRepositorySpy.user = {
     password: 'hashed_password'
   }
 
+  return loadUserByEmailRepositorySpy
+}
+
+const makeSut = () => {
+  const encrypterSpy = makeEncrypter()
+  const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository()
+
   const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy)
 
   return {
     sut,
-    loadUserByEmailRepositorySpy,
-    encrypterSpy
+    encrypterSpy,
+    loadUserByEmailRepositorySpy
   }
 }
 
@@ -70,7 +85,8 @@ describe('Auth UseCase', () => {
   })
 
   test('Should returns null if an invalid password is provided', async () => {
-    const { sut } = makeSut()
+    const { sut, encrypterSpy } = makeSut()
+    encrypterSpy.isValid = false
     const acessToken = await sut.auth('any_email@email.com', 'invalid_password')
     expect(acessToken).toBeNull()
   })
